7e9b0775601ccd4188d4267ef1a8e0120b362291
Yousuf Alqasim <zwgf0500@leeds.ac.uk>
2026-02-13 12:19:11 +0000

    f

--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -11,10 +11,21 @@ int main( void ) {
     char testChar;
     int testInt;
     float testFloat;
+    double testDouble;
+    short int testShortint;
+    long int testLongint;
 
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
+    printf("Short int is %ld bytes\n",sizeof(testShortint));
+    printf("Double is %ld bytes\n",sizeof(testDouble));
+    printf("Char size is %ld bytes\n",sizeof(testChar));
+    printf("Int size is %ld bytes\n",sizeof(testInt));
+    printf("Float size is %ld bytes\n",sizeof(testFloat));
+    printf("Short int is %ld bytes\n",sizeof(testShortint));
+    printf("Long int is %ld bytes\n",sizeof(testLongint));
+
 
     return 0;
 }
